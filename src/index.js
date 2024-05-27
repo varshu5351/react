cb66ed1b35e91f51def07dbb50534fc64436c44d
varshitha <varshitha264@gmail.com>
2024-05-28 00:36:07 +0530

    hi

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import './index.css';
+// import App from './App';
+// // import reportWebVitals from './reportWebVitals';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import store from './redux/store';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <>
+//     <React.StrictMode>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </React.StrictMode>
+//   </>
+// );
+
+
+// // index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import store from './redux/store'; // Assuming you have your Redux store configured properly
+// import App from './App';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <Router>
+//       <App />
+//     </Router>
+//   </Provider>,
+//   document.getElementById('root')
+// );
